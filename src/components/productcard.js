d76b98c0efb865c74d11a026288e0c121d3515bf
matiasistu <matute.mati@hotmail.com>
2023-10-29 17:35:36 -0300

    Agrego tidio

--- src/components/productcard.js
+++ src/components/productcard.js
@@ -1,35 +1,52 @@
 import React from 'react';
-import { Box, Text, Image, Badge } from '@chakra-ui/react';
+import { Box, Text, Image, Badge, VStack } from '@chakra-ui/react';
 import tortadechocolate from '../images/torta_chocolate.jpg'
 const ProductCard = ({ product }) => {
   console.log(product.image)
   return (
-    <Box
-      key={product.id}
-      borderWidth="1px"
-      borderRadius="lg"
-      overflow="hidden"
-      boxShadow="md"
-      maxW="250px"
-      maxH="280px"
-      minH="280px"
-      minW="250px"
+<Box
+  key={product.id}
+  borderWidth="1px"
+  borderRadius="lg"
+  overflow="hidden"
+  boxShadow="md"
+  maxW="250px"
+  maxH="350px"
+  minH="350px"
+  minW="250px"
+  display="flex"
+  flexDirection="column"
+  justifyContent="space-between"
+>
 
+  <Image src={tortadechocolate} alt={product.name} h="50%" />
+
+  <Box p="4" textAlign="center" flexGrow={1} display="flex" flexDirection="column" justifyContent="center">
+    <Text fontSize="lg" fontWeight="semibold">
+      {product.name}
+    </Text>
+    <Text fontSize="sm" color="gray.500">
+      {product.description}
+    </Text>
+  </Box>
+
+  <Box p="4" textAlign="center">
+    <Badge
+      borderRadius="full"
+      px="3"
+      colorScheme="blue"
+      variant="solid"
+      fontSize="lg"
+      style={{ alignSelf: 'flex-end' }} // Esto alinea el precio en la parte inferior de su contenedor
     >
-      <Image src={tortadechocolate} alt={product.name} />
-      <Text ml='3' mt="2" fontSize="lg" fontWeight="semibold">
-        {product.name}
-      </Text>
-      <Text ml='3' fontSize="sm" color="gray.500">
-        {product.description}
-      </Text>
-      {/* <Box mt="auto"> 
-        <Badge borderRadius="full" px="2" colorScheme="teal">
-          ${product.price}
-        </Badge>
-      </Box> */}
+      ${product.price}
+    </Badge>
+  </Box>
 
-    </Box>
+</Box>
+
+
+
   );
 };
 
