9e988d9152a317a58932d5f3e40dbc583498b3bc
matiasistu <matute.mati@hotmail.com>
2023-11-21 09:42:16 -0300

    Contacto

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -14,7 +14,7 @@ function Contacto() {
             <VStack spacing="4" align="stretch" maxW="400px" m="auto">
               <Input type="text" placeholder="Nombre" />
               <Input type="email" placeholder="Correo Electrónico" />
-              <Textarea placeholder="Mensaje" resize="vertical" />
+              <Textarea placeholder="Mensaje" resize="vertical" h="700px" />
               <Button colorScheme="teal" type="submit" w="100%">
                 Enviar Mensaje
               </Button>
@@ -23,21 +23,23 @@ function Contacto() {
         </Box>
 
         <Box>
-          <Heading as="h2" mb="4">
-            O comunicate con nosotros por WhatsApp
-          </Heading>
-          <Button
-            size="lg"
-            width="88px"
-            borderRadius="30"
-            colorScheme="whatsapp"
-            as="a"
-            href="https://wa.me/1234567890"  // Reemplaza con tu enlace de WhatsApp
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaWhatsapp size="50" style={{ color: 'white' }} />
-          </Button>
+          <Flex direction="row" align="center" justifyContent="space-around">
+            <Heading as="h2" mb="4">
+              O comunicate con nosotros por WhatsApp
+            </Heading>
+            <Button
+              size="lg"
+              width="88px"
+              borderRadius="30"
+              colorScheme="whatsapp"
+              as="a"
+              href="https://wa.me/1234567890"  // Reemplaza con tu enlace de WhatsApp
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaWhatsapp size="50" style={{ color: 'white' }} />
+            </Button>
+          </Flex>
         </Box>
       </Grid>
     </Box>
